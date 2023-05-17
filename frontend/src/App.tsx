1a6663be97b7c75e2491486edb6e4dff1dfdad2e
Israel <israelfelipe9@gmail.com>
2023-05-16 22:14:57 -0300

    Navbar and Forms First commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,20 @@
 import LoginForm from "./components/loginForm";
 import RegisterForm from "./components/registerForm";
+import NavBar from "./components/navBar";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/login" element={<LoginForm />} />
-      <Route path="/register" element={<RegisterForm />} />
-    </Routes>
+    <>
+      <NavBar user={"Israel"} />
+      <main className="container">
+        <Routes>
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/register" element={<RegisterForm />} />
+        </Routes>
+      </main>
+    </>
   );
 }
 
